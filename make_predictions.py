a7a21268aece721020c9366c9fb595249784ce8c
Heather Ciallella <heather.ciallella@rutgers.edu>
2021-03-17 16:26:55 -0400

    make model selection more adjustable

--- make_predictions.py
+++ make_predictions.py
@@ -17,58 +17,68 @@ parser.add_argument('-f', '--features', metavar='f', type=str, help='features to
 parser.add_argument('-nc', '--name_col', metavar='nc', type=str, help='name of name column in sdf file')
 parser.add_argument('-ps', '--prediction_set', metavar='ps', type=str, help='prediction set name')
 parser.add_argument('-t', '--threshold', metavar='t', type=int, help='toxic threshold')
-parser.add_argument('-di', '--dont_include', metavar='dir', type=str, help='models not to include...should be a csv '
-                                                                           'string')
+parser.add_argument('-a', '--algorithms', metavar='dir', type=str, help='models to include...should be a csv string')
 
 args = parser.parse_args()
 data_dir = os.getenv(args.env_var)
 env_var = args.env_var
-features = args.features.split(',')
+features = args.features
 name_col = args.name_col
 prediction_set = args.prediction_set
 endpoint = args.endpoint
 threshold = args.threshold
-evaluate = args.evaluate
 train_name = args.train_name
-dont_include = args.dont_include.split(',')
+algorithms = args.algorithms.lower().split(',')
 
-X_pred = make_dataset(f'{train_name}.sdf', data_dir=data_dir, features=features, name_col=name_col, endpoint=endpoint,
+X_pred = make_dataset(f'{prediction_set}.sdf', data_dir=env_var, features=features, name_col=name_col, endpoint=endpoint,
                       threshold=threshold, pred_set=True)
-algorithms = [alg for alg in ['ada', 'bnb', 'knn', 'nb', 'rf', 'svc'] if alg not in dont_include]
+algorithms = [alg for alg in algorithms]
 preds = []
+
+if len(algorithms) < 1:
+    raise Exception('Please enter at least one algorithm with which to make predictions.')
 
 for alg in algorithms:
     model_name = f'{alg}_{train_name}_{features}_{endpoint}_{threshold}_pipeline'
-    model_file_path = os.path.join(data_dir, 'ML_models', f'{model_name}.pkl')
+    model_file_path = os.path.join(data_dir, 'models', f'{model_name}.pkl')
 
     if os.path.exists(model_file_path):
         loaded_model = load(model_file_path)
-        probabilities = loaded_model.predict_proba(X_pred)
+        probabilities = pd.Series(loaded_model.predict_proba(X_pred)[:, 1], index=X_pred.index)
         preds.append(probabilities)
 
-concatenated = pd.concat(preds, axis=0)
-consensus_preds = concatenated.groupby(concatenated.index).mean()
-preds.append(consensus_preds)
-algorithms.append('consensus')
+if len(preds) > 1:
+    concatenated = pd.concat(preds, axis=0)
+    consensus_preds = concatenated.groupby(concatenated.index).mean()
+    consensus_preds[consensus_preds >= 0.5] = 1
+    consensus_preds[consensus_preds < 0.5] = 0
+    final_preds = consensus_preds
 
-final_preds = pd.concat(preds, axis=1)
-final_preds.columns = algorithms
-final_preds[final_preds >= 0.5] = 1
-final_preds[final_preds < 0.5] = 0
+else:
+    final_preds = preds[0]
 
-X, y = make_dataset(f'{train_name}.sdf', data_dir=data_dir, features=features, name_col=name_col, endpoint=endpoint,
-                    threshold=threshold)
-y = y.loc[X_pred.index]
-y[y.isnull()] = consensus_preds
-y[y in ['nan', 'NaN']] = consensus_preds
+molecules = generate_molecules(prediction_set, data_dir, endpoint)
 
-molecules = generate_molecules(os.path.join(data_dir, f'{train_name}.sdf'))
+if len([mol for mol in molecules if mol.HasProp(endpoint)]) > 0:
+    X, y = make_dataset(f'{prediction_set}.sdf', data_dir=env_var, features=features, name_col=name_col, endpoint=endpoint,
+                        threshold=threshold, cache=False)
+    y = y.reindex(X_pred.index)
+    y[y.isnull()] = final_preds
 
-for molecule in molecules:
-    if not molecule.HasProp(endpoint):
-        molecule.SetProp(endpoint, str(y.loc[molecule.GetProp(name_col)]))
+    y.to_csv(os.path.join(
+        data_dir, 'predictions', f'{prediction_set}_{features}_{endpoint}_{threshold}_no_gaps.csv'),
+        header=['Activities'])
 
-w = SDWriter(os.path.join(data_dir, f'{train_name}_with_predictions.sdf'))
+    for molecule in molecules:
+        if not molecule.HasProp(endpoint):
+            molecule.SetProp(endpoint, str(y.loc[molecule.GetProp(name_col)]))
+
+else:
+    for molecule in molecules:
+        molecule.SetProp(endpoint, final_preds.loc[molecule.GetProp(name_col)])
+
+
+w = SDWriter(os.path.join(data_dir, f'{prediction_set}_with_predictions.sdf'))
 
 for molecule in molecules:
     w.write(molecule)
